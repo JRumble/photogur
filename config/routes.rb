1da0da712222aca00d913531a004344a0f7c5f19
JRumble <jody.paget@rogers.com>
2015-02-03 17:51:16 -0500

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
 
 root 'pictures#index'
 
-get 'pictures' => 'pictures#index' 
-post 'pictures' => 'pictures#create' 
-get 'pictures/new' => 'pictures#new'
-
-get 'pictures/:id' => 'pictures#show' 
+resources :pictures
 
 end
 
